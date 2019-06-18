d19a93960d22b9ff1700059dc448adf9990ce72d
sunjin7725 <sunjin7725@gmail.com>
2019-06-18 20:38:21 +0900

    #9 Authentication is complete

--- src/passtport.js
+++ src/passtport.js
@@ -1,21 +1,33 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path : path.resolve(__dirname,".env")});
-
+import "./env";
 import passport from "passport";
-import JWTStrategy from "passport-jwt";
+import {Strategy, ExtractJwt} from "passport-jwt";
+import { prisma } from "../generated/prisma-client";
 
 const jwtOptions = {
-    jwtFromRequest: JWTStrategy.ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secret: process.env.JWT_SECRET
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET
 };
 
-const verifyUser = (payload, done)=>{
+const verifyUser = async(payload, done)=>{
     try{
-
+        const user = await prisma.user({id:payload.id});
+        if(user !== null){
+            return done(null,user);
+        }
+        else{
+            return done(null, false);
+        }
     }catch(e){
-        
+        return done(e,false);
     }
 };
 
-passport.use(new JWTStrategy(jwtOptions, verifyUser));
+export const authenticateJwt = (req, res, next) => passport.authenticate("jwt",{sessions:false},(error, user)=>{
+    if(user){
+        req.user = user;
+    }
+    next();
+})(req, res, next);
+
+passport.use(new Strategy(jwtOptions, verifyUser));
+passport.initialize();
--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path : path.resolve(__dirname,".env")});
-
+import "./env";
 import {GraphQLServer} from "graphql-yoga";
 import logger from "morgan";
-import schema from "./schema"
+import schema from "./schema";
+import "./passtport";
+import { authenticateJwt } from "./passtport";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({ schema, context:({request})=>({request}) });
 
-//server.express.use(logger("dev"));
+server.express.use(logger("dev"));
+server.express.use(authenticateJwt);
 
 server.start({port:PORT}, () => console.log(`Server running on port http://localhost:${PORT}`));
