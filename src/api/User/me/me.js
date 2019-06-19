4ebbdf72ee59666143353a0f799e6d8387a2cde2
sunjin7725 <sunjin7725@gmail.com>
2019-06-19 18:06:31 +0900

    #14 See Full Post & How to make fragments

---
+++ src/api/User/me/me.js
@@ -0,0 +1,19 @@
+import { isAuthenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+import { USER_FRAGMENT } from "../../../fragments";
+
+export default {
+    Query: {
+        me: async(_, args, { request }) => {
+            isAuthenticated(request);
+            const { user } = request;
+            const userProfile = await prisma.user({ id: user.id });
+            const posts = await prisma.user({ id: user.id }).posts();
+
+            return {
+                user: userProfile,
+                posts
+            };
+        }
+    }
+}
